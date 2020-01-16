09406b97074065a2896730cf14dcf8257283c9c1
zxg <329974342@qq.com>
2020-01-16 15:02:35 +0800

    测试push

--- src/main/java/com/mpfrost/test/controller/HelloController.java
+++ src/main/java/com/mpfrost/test/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Hello Controller
  * @author zxg
  * @since 2020-01-15 16:38:54
  */
