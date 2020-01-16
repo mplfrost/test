1a51ed4695d3f97d4e08440d11c335579d98a947
zxg <329974342@qq.com>
2020-01-16 15:06:50 +0800

    测试push2

--- src/main/java/com/mpfrost/test/controller/HelloController.java
+++ src/main/java/com/mpfrost/test/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld!!!";
