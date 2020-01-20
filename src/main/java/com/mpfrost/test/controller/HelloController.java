1c81ceac2448d21944cdced368136933acf425d3
zxg <329974342@qq.com>
2020-01-20 10:42:15 +0800

    添加docker插件

--- src/main/java/com/mpfrost/test/controller/HelloController.java
+++ src/main/java/com/mpfrost/test/controller/HelloController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-    
+
     @GetMapping("/hello")
     public String hello() {
-        return "HelloWorld!!!";
+        return "HelloWorld---1!!!";
     }
 }
